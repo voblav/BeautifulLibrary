b101313dda274c20bc247b263d48d42dcb77abe2
voblav <35613203+voblav@users.noreply.github.com>
2018-07-13 10:54:43 +0300

    Upgrated

--- BtfLibrary03/BtfLibrary.UI/Controllers/BooksController.cs
+++ BtfLibrary03/BtfLibrary.UI/Controllers/BooksController.cs
@@ -1,25 +1,40 @@
-﻿using BtfLibrary.Domain.Abstract;
+﻿using Domain.Abstract;
+using Lib.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BtfLibrary.UI.Controllers
+namespace Lib.UI.Controllers
 {
     public class BooksController : Controller
     {
         private IBookRepository repository;
         public int pageSize = 4;
+
         public BooksController(IBookRepository repo)
         {
             repository = repo;
         }
-
         public ViewResult List(int page = 1)
         {
+            BooksListViewModel model = new BooksListViewModel
+            {
+                Books = repository.Books
+                .OrderBy(book => book.BookID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Books.Count()
+                }
+            };
+
             return View(repository.Books
-                .OrderBy(book => book.IdBook)
+                .OrderBy(book => book.BookID)
                 .Skip((page - 1)*pageSize)
                 .Take(pageSize));
         }
