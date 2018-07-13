c4d496f4228bb1673ed7c171d01ba330ed76b5a6
voblav <35613203+voblav@users.noreply.github.com>
2018-07-13 11:01:15 +0300

    *

---
+++ BtfLibrary03/BtfLibrary.UI/Models/BooksListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities;
+
+namespace Lib.UI.Models
+{
+    public class BooksListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+
+    }
+}
--- BtfLibrary03/BtfLibrary.UI/Models/PagingInfo.cs
+++ BtfLibrary03/BtfLibrary.UI/Models/PagingInfo.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace BtfLibrary.UI.Models
+namespace Lib.UI.Models
 {
     public class PagingInfo
     {
         public int TotalItems { get; set; }
-        public int ItemsPerPage { get; set;}
+        public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages
         {
